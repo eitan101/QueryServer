a3c0ddeec62362d5a188a047c0781557e1a383c9
eitan101 <eitan101@gmail.com>
2014-03-25 18:16:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+QueryServerEmbedded
+===================
+Ensure that JAVA_HOME points to jdk8. cd to the project dir and run with: 
+
+    gradlew.bat 
+or 
+
+    ./gradlew
+open http://localhost:8080/ and then press connect button
+
+For standalone run myTest class
---
+++ src/main/java/co/eitan101/examples/JettyServer.java
@@ -0,0 +1,53 @@
+package co.eitan101.examples;
+
+import events.Utils;
+import javax.websocket.server.ServerContainer;
+import org.eclipse.jetty.server.Handler;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+public class JettyServer {
+
+    public static void main(String[] args) {
+        FullPmQueryServerExample.getPmQueryServer().put("default", Utils.xpath("starts-with(subEntities/target/name,'f')"));
+        runHttpServer(WsTest.class, "index.html");
+        System.out.println("open http://localhost:8080/ in browser console type connect()");
+    }
+
+    private static void runHttpServer(Class<WsTest> wsClass, String welcomeFile) {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        final ResourceHandler rh = new ResourceHandler();
+        rh.setDirectoriesListed(true);
+        rh.setWelcomeFiles(new String[]{ welcomeFile});
+        rh.setResourceBase(".");
+        final HandlerList handlers = new HandlerList();
+        final ContextHandler resourceCtx = new ContextHandler();
+        resourceCtx.setContextPath("/");
+        resourceCtx.setHandler(rh);
+        ServletContextHandler wsContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        wsContext.setContextPath("/data");
+        handlers.setHandlers(new Handler[] { wsContext,resourceCtx,new DefaultHandler()});
+        server.setHandler(handlers);
+
+        try {
+            ServerContainer wscontainer = WebSocketServerContainerInitializer.configureContext(wsContext);
+            wscontainer.addEndpoint(wsClass);
+            server.start();
+            server.dump(System.err);
+            server.join();
+        } catch (Exception t) {
+            t.printStackTrace(System.err);
+        }
+    }
+}
